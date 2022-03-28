cf0c3ebd3585ca363c3032de2499ae2496053160
Rohan Lahori <19itroh049@ldce.ac.in>
2022-03-28 11:45:50 +0530

    commit

--- Weather_Application/main.js
+++ Weather_Application/main.js
@@ -1,1 +1,12 @@
-console.log('Weather App')
+const express=require('express')
+const app=express()
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.post('/weather',(req,res)=>{
+    res.send('Weather application')
+})
+app.listen(3000,()=>{
+    console.log('User is listening on this port')
+})
