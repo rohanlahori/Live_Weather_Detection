18ef97d6cd58c34a674f76bcf52f613b8c5150cf
Rohan Lahori <19itroh049@ldce.ac.in>
2022-04-01 16:10:41 +0530

    commit

--- Weather_Application/static_files/index.js
+++ Weather_Application/static_files/index.js
@@ -4,13 +4,13 @@ function submit()
     console.log(city_name);
     const url="https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid=5122ecc1ab7b13a5a41d8e7dcbfcd709";
     getapi(url);
-    console.log(url)
+    // console.log(url)
     // console.clear()
 }
 async function getapi(url) {
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data)
+    // console.log(data)
     var cur_temp=document.querySelector('#cur_temp')
     cur_temp.innerText=Math.round(data.main.temp-273.15)+"° C";
     var min_temp=document.querySelector('#min_temp')
