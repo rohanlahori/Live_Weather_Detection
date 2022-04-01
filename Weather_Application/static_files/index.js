526ef484da69edee4a8006ec0eb73a41b4876a00
Rohan Lahori <19itroh049@ldce.ac.in>
2022-04-01 16:02:35 +0530

    commit

--- Weather_Application/static_files/index.js
+++ Weather_Application/static_files/index.js
@@ -1,20 +1,27 @@
 function submit()
 {
     var city_name=document.getElementById('search').value;
+    console.log(city_name);
     const url="https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid=5122ecc1ab7b13a5a41d8e7dcbfcd709";
     getapi(url);
     console.log(url)
-    console.clear()
+    // console.clear()
 }
 async function getapi(url) {
-    console.log(url)
     const response = await fetch(url);
     var data = await response.json();
     console.log(data)
-    var temprature=document.getElementById('temprature')
-    temprature.innerText=Math.round(data.main.temp-273.15)+"° C"
-    var city=document.getElementById('search').value;
-    city.innerText=data.main.name
-    console.log(city)
+    var cur_temp=document.querySelector('#cur_temp')
+    cur_temp.innerText=Math.round(data.main.temp-273.15)+"° C";
+    var min_temp=document.querySelector('#min_temp')
+    min_temp.innerText=Math.round(data.main.temp_min-273.15)+"° C";
+    var max_temp=document.querySelector('#max_temp')
+    max_temp.innerText=Math.round(data.main.temp_max-273.15)+"° C";
+    var feels_like=document.querySelector('#feels_like')
+    feels_like.innerText=Math.round(data.main.feels_like-273.15)+"° C";
+    var city=document.querySelector('#city');
+    city.innerText=data.name
+    var description=document.querySelector('#description');
+    description.innerText=data.weather[0].description
 }
 
